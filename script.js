8eab11d80d7199ca4eaf020132ed43944e0993e3
Saurabh Singh Bisht <126646740+Saurabh-Singh-Bisht@users.noreply.github.com>
2024-01-29 09:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const opt = document.querySelector("#colorSelect");
+const btn = document.querySelector("input");
+btn.addEventListener("click", () => {
+	const selectVal = opt.value;
+	let option = opt.children;
+	for(let i=0;i<option.length;i++){
+		if(option[i].value === selectVal){
+			option[i].remove();
+			return;
+		}
+	}
+})
